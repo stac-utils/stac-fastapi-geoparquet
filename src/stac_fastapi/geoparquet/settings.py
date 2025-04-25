ce1e5cc91255c0010a31ef72d22bb8f568533a7c
Pete Gadomski <pete.gadomski@gmail.com>
2025-04-25 05:40:38 -0600

    docs: talk about collections

--- src/stac_fastapi/geoparquet/settings.py
+++ src/stac_fastapi/geoparquet/settings.py
@@ -4,7 +4,12 @@ from stac_fastapi.types.config import ApiSettings
 class Settings(ApiSettings):  # type: ignore
     """stac-fastapi-geoparquet settings"""
 
-    stac_fastapi_collections_href: str
-    """A file containing JSON list of collections.
+    stac_fastapi_collections_href: str | None = None
+    """The href of a file containing JSON list of collections.
 
     Any parquet assets on the collection will be loaded into the server."""
+
+    stac_fastapi_geoparquet_href: str | None = None
+    """The href of a stac-geoparquet file.
+
+    The items in the file will be used to auto-generate one or more collections."""
